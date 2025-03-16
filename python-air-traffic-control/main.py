d9f75b53df027285b7ac26b08a1faf6eed18ab91
jiayi <jiayi031209@gmail.com>
2025-03-16 14:28:39 +0800

    Cleaned game directory

--- python-air-traffic-control/main.py
+++ python-air-traffic-control/main.py
@@ -3,9 +3,6 @@
 
 from pygame import *
 from game import *
-from highs import *
-import os
-import info_logger
 import menu_base
 import conf
 
@@ -37,10 +34,10 @@ class Main:
         self.menu.from_file('main_menu')
         self.ages = menu_base.menu_base(self.screen,150,25)
         self.ages.from_file('ages_menu')
-        self.high = HighScore(self.screen)
-        self.infologger = info_logger.info_logger()
+        # self.high = HighScore(self.screen)
+        # self.infologger = info_logger.info_logger()
         #Current visitor number
-        self.id = int(self.infologger.get_id())
+        # self.id = int(self.infologger.get_id())
 
     def run(self):
         state = STATE_MENU
@@ -48,49 +45,6 @@ class Main:
         score = 0       
         game = Game(self.screen, False)
         (gameEndCode, score) = game.start()
-        
-        
-        # while (exit == 0):
-        #     if (state == STATE_MENU):
-        #         menuEndCode = None
-        #         menuEndCode = self.menu.main_loop()
-        #         self.infologger.writeout()
-        #         if (menuEndCode == conf.get()['codes']['start']):
-        #             state = STATE_AGES
-        #             self.id += 1
-        #             self.infologger.add_value(self.id,'id',self.id)
-        #         elif (menuEndCode == conf.get()['codes']['demo']):
-        #             state = STATE_DEMO
-        #         elif (menuEndCode == conf.get()['codes']['high_score']):
-        #             state = STATE_HIGH
-        #         elif (menuEndCode == conf.get()['codes']['kill']):
-        #             state = STATE_KILL
-        #     elif (state == STATE_GAME):
-        #         game = Game(self.screen, False)
-        #         (gameEndCode, score) = game.start()
-        #         self.infologger.add_value(self.id,'score',score)
-        #         if (gameEndCode == conf.get()['codes']['time_up']):
-        #             state = STATE_HIGH
-        #         elif (gameEndCode == conf.get()['codes']['kill']):
-        #             state = STATE_KILL
-        #         elif (gameEndCode == conf.get()['codes']['user_end']):
-        #             state = STATE_MENU
-        #         elif (gameEndCode == conf.get()['codes']['ac_collide']):
-        #             state = STATE_HIGH
-        #     elif (state == STATE_DEMO):
-        #        game = Game(self.screen, True)
-        #        (gameEndCode, score) = game.start()
-        #        state = STATE_MENU
-        #     elif (state == STATE_HIGH):
-        #         highEndCode = self.high.start(score)
-        #         state = STATE_MENU
-        #         score = 0
-        #     elif (state == STATE_KILL):
-        #         exit = 1
-        #     elif (state == STATE_AGES):
-        #         self.infologger.add_value(self.id,'agegroup',self.ages.main_loop())
-        #         state = STATE_GAME
-        #     game = None
 
 # main entry point
 if __name__ == '__main__':
